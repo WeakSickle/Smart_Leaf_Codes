7dceeb06d8157520de94028d850d2eabad907386
tphi219 <tphi219@aucklanduni.ac.nz>
2025-03-13 11:02:15 +1300

    Some improvements to icon change and readability

--- testing_map.py
+++ testing_map.py
@@ -1,15 +1,11 @@
-# Packages needed to run teh code 
-# PyQt5, PyQtWebEngine, folium, requests
-
-
 import sys
 import io
 import folium
 from PyQt5 import QtCore, QtWidgets, QtWebEngineWidgets
 import requests
 
-#Class for the map 
-class WebEngineView(QtWebEngineWidgets.QWebEngineView):
+# Class for interacting with the map with markers and stuff 
+class FoliumMap(QtWebEngineWidgets.QWebEngineView):
     map_loaded = QtCore.pyqtSignal()
 
     def __init__(self, parent=None):
@@ -18,10 +14,7 @@ class WebEngineView(QtWebEngineWidgets.QWebEngineView):
         self.current_map = None  # Store the Folium map reference
         
     def set_folium_map(self, coordinates):
-        # Create and store the Folium map instance
         self.current_map = folium.Map(location=coordinates, zoom_start=12)
-        
-        # Save to bytes buffer
         data = io.BytesIO()
         self.current_map.save(data, close_file=False)
         self.setHtml(data.getvalue().decode('utf-8'))
@@ -29,35 +22,31 @@ class WebEngineView(QtWebEngineWidgets.QWebEngineView):
         self.map_loaded.emit()
         
     def add_marker(self, coordinates, popup_text='Marker'):
-        # Add marker to the stored map instance
-        folium.Marker(coordinates, popup=popup_text).add_to(self.current_map)
-        
-        # Update the web view with modified map
+        folium.Marker(coordinates, popup=popup_text,tooltip=popup_text, icon=folium.Icon(color='darkblue', icon_color='white',icon='tint')).add_to(self.current_map)
         data = io.BytesIO()
         self.current_map.save(data, close_file=False)
         self.setHtml(data.getvalue().decode('utf-8'))
 
-# Main GUI stuff that would be replaced with other GUI code
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Folium Map')
         self.setGeometry(100, 100, 800, 600)
         
-        # Initialize web view
-        self.web_view = WebEngineView()
-        self.setCentralWidget(self.web_view)
+        self.map = FoliumMap()
+        self.setCentralWidget(self.map)
         
-        # Get location from IP - from your thing
         response = requests.get('https://ipinfo.io')
         data = response.json()
         latitude, longitude = map(float, data['loc'].split(','))
         
-        # Load map after making the window
-        QtCore.QTimer.singleShot(100, lambda: (self.web_view.set_folium_map((latitude, longitude)),
-            # Add marker after map loads this method using a 500ms delay 
-            QtCore.QTimer.singleShot(500, lambda: self.web_view.add_marker((latitude, longitude), "Device 1"))
+        QtCore.QTimer.singleShot(100, lambda: (
+            self.map.set_folium_map((latitude, longitude)),
+            QtCore.QTimer.singleShot(500, lambda: self.map.add_marker((latitude, longitude), "Device 1"))
         ))
+    
+    def Update_markers(self, coordinates, popup_text):
+        self.map.add_marker(coordinates, popup_text)
 
 if __name__ == '__main__':
     QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
@@ -67,7 +56,13 @@ if __name__ == '__main__':
     window = MainWindow()
     window.show()
 
-    # Adding a marker through main (this waits till map is loaded to add marker)
-    window.web_view.map_loaded.connect(lambda: window.web_view.add_marker((-36.89, 174.72), "Device 2"))
+    window.map.map_loaded.connect(lambda: window.map.add_marker((-36.89, 174.72), "Device 2"))
+
+    # Handle 'U' key press
+    window.keyPressEvent = lambda event: window.Update_markers((-35, 174), "Device 5") if event.key() == 85 else None
+
+    # Add a marker after 5 seconds (simulated delayed serial data)
+    QtCore.QTimer.singleShot(5000, lambda: window.Update_markers((-36, 174), "Device 6"))
+
 
     sys.exit(app.exec_())
