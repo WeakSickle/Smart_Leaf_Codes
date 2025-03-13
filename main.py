2b38720bf671b6df80625b2ea2a1a48b9983a789
Tiernan Phillips <139313158+tphi219@users.noreply.github.com>
2025-03-14 10:27:14 +1300

    Added Map functionaility

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 from HomeUI import UI_Home
 from MapUI3 import UI_Map
+from map_interface import FoliumMap
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -23,12 +23,14 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def switch_to_map(self):
         # Switch to second layout
-        self.ui_map = UI_Map()
+        self.ui_map = FoliumMap()
         self.setGeometry(100,100,800,600)
         self.setCentralWidget(self.ui_map)
 
 if __name__ == "__main__":
-    import sys
+    
+    
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()  # Instantiate MainWindow
+
     sys.exit(app.exec_())  # Run the application
