e5bae5fee67ab3a46f6a8fbf443fac5bb98b62f9
Ivan Santiago <isan182@UoA.auckland.ac.nz>
2025-05-24 17:56:54 +1200

    Implemented test to sleep LoRa for a set time

--- LILYGO T BEAM/custom_test_scripts/LORA_SLEEP/LoRa_TSLEEP.ino
+++ LILYGO T BEAM/custom_test_scripts/LORA_SLEEP/LoRa_TSLEEP.ino
@@ -387,7 +387,7 @@ void loop()
         */
 
         // Put radio and MCU to sleep
-        sleepDevice(15); // Sleep for 15 seconds
+        sleepDevice(1); // Sleep for # seconds
 
     }
 }
@@ -422,27 +422,152 @@ void sleepDevice(uint32_t sleepSeconds) {
   
   // Put the radio to sleep
   radio.sleep();
+
+
+//   // Put MCU to sleep (implementation depends on your board)
+//   // ESP32/ESP8266 version:
+//   #if defined(ESP32) || defined(ESP8266)
+//     esp_sleep_enable_timer_wakeup(sleepSeconds * 1000000);
+//     esp_deep_sleep_start();
   
-  // Put MCU to sleep (implementation depends on your board)
-  // ESP32/ESP8266 version:
-  #if defined(ESP32) || defined(ESP8266)
-    esp_sleep_enable_timer_wakeup(sleepSeconds * 1000000);
-    esp_deep_sleep_start();
-  
-  // Arduino AVR version:
-  #else
-    for (uint32_t i = 0; i < sleepSeconds; i++) {
-      LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF); 
-    }
-  #endif
-  
-  // When we wake up
-  Serial.println(F("Waking up!"));
+//   // Arduino AVR version:
+//   #else
+//     for (uint32_t i = 0; i < sleepSeconds; i++) {
+//       LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF); 
+//     }
+//   #endif
+
+    
+  delay(5000);
   
   // Reinitialize radio after sleep
-  radio.begin();
+  int state = radio.begin();
   // Reapply your radio settings if needed
-  radio.setFrequency(CONFIG_RADIO_FREQ);
+  //radio.setFrequency(CONFIG_RADIO_FREQ);
   
-  setup(); // regain all settings
+  //setup(); // regain all settings
+
+  // When we wake up
+  Serial.println(F("Waking up!"));
+
+  printResult(state == RADIOLIB_ERR_NONE);
+
+    Serial.print(F("Radio Initializing ... "));
+    if (state == RADIOLIB_ERR_NONE) {
+        Serial.println(F("success!"));
+    } else {
+        Serial.print(F("failed, code "));
+        Serial.println(state);
+        while (true);
+    }
+
+    // set the function that will be called
+    // when packet transmission is finished
+    radio.setPacketSentAction(setFlag);
+
+    /*
+    *   Sets carrier frequency.
+    *   SX1278/SX1276 : Allowed values range from 137.0 MHz to 525.0 MHz.
+    *   SX1268/SX1262 : Allowed values are in range from 150.0 to 960.0 MHz.
+    *   SX1280        : Allowed values are in range from 2400.0 to 2500.0 MHz.
+    *   LR1121        : Allowed values are in range from 150.0 to 960.0 MHz, 1900 - 2200 MHz and 2400 - 2500 MHz. Will also perform calibrations.
+    * * * */
+
+    if (radio.setFrequency(CONFIG_RADIO_FREQ) == RADIOLIB_ERR_INVALID_FREQUENCY) {
+        Serial.println(F("Selected frequency is invalid for this module!"));
+        while (true);
+    }
+
+    /*
+    *   Sets LoRa link bandwidth.
+    *   SX1278/SX1276 : Allowed values are 10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125, 250 and 500 kHz. Only available in %LoRa mode.
+    *   SX1268/SX1262 : Allowed values are 7.8, 10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125.0, 250.0 and 500.0 kHz.
+    *   SX1280        : Allowed values are 203.125, 406.25, 812.5 and 1625.0 kHz.
+    *   LR1121        : Allowed values are 62.5, 125.0, 250.0 and 500.0 kHz.
+    * * * */
+    if (radio.setBandwidth(CONFIG_RADIO_BW) == RADIOLIB_ERR_INVALID_BANDWIDTH) {
+        Serial.println(F("Selected bandwidth is invalid for this module!"));
+        while (true);
+    }
+
+
+    /*
+    * Sets LoRa link spreading factor.
+    * SX1278/SX1276 :  Allowed values range from 6 to 12. Only available in LoRa mode.
+    * SX1262        :  Allowed values range from 5 to 12.
+    * SX1280        :  Allowed values range from 5 to 12.
+    * LR1121        :  Allowed values range from 5 to 12.
+    * * * */
+    if (radio.setSpreadingFactor(12) == RADIOLIB_ERR_INVALID_SPREADING_FACTOR) {
+        Serial.println(F("Selected spreading factor is invalid for this module!"));
+        while (true);
+    }
+
+    /*
+    * Sets LoRa coding rate denominator.
+    * SX1278/SX1276/SX1268/SX1262 : Allowed values range from 5 to 8. Only available in LoRa mode.
+    * SX1280        :  Allowed values range from 5 to 8.
+    * LR1121        :  Allowed values range from 5 to 8.
+    * * * */
+    if (radio.setCodingRate(6) == RADIOLIB_ERR_INVALID_CODING_RATE) {
+        Serial.println(F("Selected coding rate is invalid for this module!"));
+        while (true);
+    }
+
+    /*
+    * Sets LoRa sync word.
+    * SX1278/SX1276/SX1268/SX1262/SX1280 : Sets LoRa sync word. Only available in LoRa mode.
+    * * */
+    if (radio.setSyncWord(0xAB) != RADIOLIB_ERR_NONE) {
+        Serial.println(F("Unable to set sync word!"));
+        while (true);
+    }
+
+    /*
+    * Sets transmission output power.
+    * SX1278/SX1276 :  Allowed values range from -3 to 15 dBm (RFO pin) or +2 to +17 dBm (PA_BOOST pin). High power +20 dBm operation is also supported, on the PA_BOOST pin. Defaults to PA_BOOST.
+    * SX1262        :  Allowed values are in range from -9 to 22 dBm. This method is virtual to allow override from the SX1261 class.
+    * SX1268        :  Allowed values are in range from -9 to 22 dBm.
+    * SX1280        :  Allowed values are in range from -18 to 13 dBm. PA Version range : -18 ~ 3dBm
+    * LR1121        :  Allowed values are in range from -17 to 22 dBm (high-power PA) or -18 to 13 dBm (High-frequency PA)
+    * * * */
+    if (radio.setOutputPower(CONFIG_RADIO_OUTPUT_POWER) == RADIOLIB_ERR_INVALID_OUTPUT_POWER) {
+        Serial.println(F("Selected output power is invalid for this module!"));
+        while (true);
+    }
+
+#if !defined(USING_SX1280) && !defined(USING_LR1121) && !defined(USING_SX1280PA)
+    /*
+    * Sets current limit for over current protection at transmitter amplifier.
+    * SX1278/SX1276 : Allowed values range from 45 to 120 mA in 5 mA steps and 120 to 240 mA in 10 mA steps.
+    * SX1262/SX1268 : Allowed values range from 45 to 120 mA in 2.5 mA steps and 120 to 240 mA in 10 mA steps.
+    * NOTE: set value to 0 to disable overcurrent protection
+    * * * */
+    if (radio.setCurrentLimit(140) == RADIOLIB_ERR_INVALID_CURRENT_LIMIT) {
+        Serial.println(F("Selected current limit is invalid for this module!"));
+        while (true);
+    }
+#endif
+
+    /*
+    * Sets preamble length for LoRa or FSK modem.
+    * SX1278/SX1276 : Allowed values range from 6 to 65535 in %LoRa mode or 0 to 65535 in FSK mode.
+    * SX1262/SX1268 : Allowed values range from 1 to 65535.
+    * SX1280        : Allowed values range from 1 to 65535. preamble length is multiple of 4
+    * LR1121        : Allowed values range from 1 to 65535.
+    * * */
+    if (radio.setPreambleLength(16) == RADIOLIB_ERR_INVALID_PREAMBLE_LENGTH) {
+        Serial.println(F("Selected preamble length is invalid for this module!"));
+        while (true);
+    }
+
+    // Enables or disables CRC check of received packets.
+    if (radio.setCRC(false) == RADIOLIB_ERR_INVALID_CRC_CONFIGURATION) {
+        Serial.println(F("Selected CRC is invalid for this module!"));
+        while (true);
+    }
+
+    // Temp for understanding
+    transmittedFlag = true; // Set flag to true to send the first packet after waking up
+
 }
