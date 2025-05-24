43673e0a64e840457fbae67082fdf9cae03fa212
tphi219 <tphi219@aucklanduni.ac.nz>
2025-05-24 23:27:48 +1200

    Started adding in the basic lora radio setup for use when wanting to network both and process real data

--- LILYGO T BEAM/mainBaseNode-Gateway/gateway.ino
+++ LILYGO T BEAM/mainBaseNode-Gateway/gateway.ino
@@ -9,6 +9,29 @@
 #include <Shared_Attribute_Update.h> 
 #include <ThingsBoard.h>
 
+// Radio Parameters
+#define USE_RADIO // If using the lora radio 
+#define CONFIG_RADIO_FREQ 850.0
+#define CONFIG_RADIO_OUTPUT_POWER 22
+#define CONFIG_RADIO_BW 125.0
+
+static volatile bool transmittedFlag = false; // Flag to indicate that a packet was received
+static volatile bool receivedFlag = false; // Flag to indicate that a packet was received
+void setReceiveFlag(void)
+{
+  // we got a packet, set the flag
+  receivedFlag = true;
+}
+
+void setTransmitFlag(void)
+{
+  // we got a packet, set the flag
+  transmittedFlag = true;
+}
+SX1262 radio = new Module(RADIO_CS_PIN, RADIO_DIO1_PIN, RADIO_RST_PIN, RADIO_BUSY_PIN);
+
+// ####### CONFIGURATION FOR WIFI AND CONNECTION TO THINGSBOARD SHIT ##########
+
 // WiFi name and password (will have to change depending on network)
 const char* ssid = "Cepter";
 const char* password = "T0dayIS7";
@@ -97,9 +120,7 @@ void requestSharedAttribute() {
   Serial.println("[MQTT] Requested shared attribute: samplingFrequency");
 }
 
-
-
-// Connects to teh virtual devices through the gateway 
+// Connects to the virtual devices through the gateway 
 void connectDevice(const char* deviceName) {
   String payloadStr = String("{\"device\":\"") + deviceName + "\"";
   const uint8_t* payload = (const uint8_t*)payloadStr.c_str();
@@ -114,8 +135,13 @@ void connectDevice(const char* deviceName) {
   }
 }
 
+
+
 // This is where potentially the readings of are formated from lora and sent
-void sendTelemetry() {
+void RecieveAndSendTelemetry() {
+ // !! REPLACE THE RANDOM BULLSHIT VALUES WITH THE TRANSMISSION ADN RECIEVE OF LORA MESSAGES !!
+
+
   // Generate random temps for each device (e.g., 20 to 30 degrees)
   int tempA = random(20, 31);
   int tempB = random(20, 31);
@@ -135,7 +161,7 @@ const float longB = -74.0060;
 const float latC = 51.5074;
 const float longC = -0.1278;
 
-// Build JSON payload dynamically using String
+// Build JSON payload dynamically using String - probably a better way to do this
 String payload = "{\n";
 payload += "  \"Device A\": [{\"temp\": " + String(tempA) 
          + ", \"battery\": " + String(batteryA) 
@@ -154,9 +180,9 @@ payload += "  \"Device C\": [{\"temp\": " + String(tempC)
 
 payload += "}";
 
-   const uint8_t* payloadBytes = (const uint8_t*)payload.c_str();
+  const uint8_t* payloadBytes = (const uint8_t*)payload.c_str();
   size_t payloadLength = payload.length();
-  // Publish the telemetry data for all teh stuff
+  // Publish the telemetry data for all the stuff
   if (client.publish("v1/gateway/telemetry", payloadBytes, payloadLength)) {
     Serial.println("Telemetry sent successfully.");
     Serial.print("Payload size: ");
@@ -174,6 +200,85 @@ void setup() {
   setupBoards();
   connectToWiFi();
   
+  // Checks for setting up the LoRA transmitter
+  // initialize radio with default settings
+  #ifdef USE_RADIO
+    int state = radio.begin();
+
+    printResult(state == RADIOLIB_ERR_NONE);
+
+    Serial.print(F("Radio Initializing ... "));
+    if (state == RADIOLIB_ERR_NONE)
+    {
+      Serial.println(F("success!"));
+    }
+    else
+    {
+      Serial.print(F("failed, code "));
+      Serial.println(state);
+      while (true)
+        ;
+    }
+      // set the function that will be called
+  // when packet transmission is finished
+
+    radio.setPacketSentAction(setTransmitFlag);
+
+    radio.setPacketReceivedAction(setReceiveFlag);
+  
+  // Sets carrier frequency. SX1268/SX1262 : Allowed values are in range from 150.0 to 960.0 MHz.
+  if (radio.setFrequency(CONFIG_RADIO_FREQ) == RADIOLIB_ERR_INVALID_FREQUENCY)
+  {
+    Serial.println(F("Selected frequency is invalid for this module!"));
+    while (true)
+      ;
+  }
+
+  // Sets LoRa link bandwidth. SX1268/SX1262 : Allowed values are 7.8, 10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125.0, 250.0 and 500.0 kHz.
+  if (radio.setBandwidth(CONFIG_RADIO_BW) == RADIOLIB_ERR_INVALID_BANDWIDTH)
+  {
+    Serial.println(F("Selected bandwidth is invalid for this module!"));
+    while (true)
+      ;
+  }
+
+  /* Sets LoRa link spreading factor. SX1262:  Allowed values range from 5 to 12. * */
+  if (radio.setSpreadingFactor(12) == RADIOLIB_ERR_INVALID_SPREADING_FACTOR)
+  {
+    Serial.println(F("Selected spreading factor is invalid for this module!"));
+    while (true)
+      ;
+  }
+
+  // Sets LoRa coding rate denominator. SX1278/SX1276/SX1268/SX1262 : Allowed values range from 5 to 8. Only available in LoRa mode.
+  if (radio.setCodingRate(6) == RADIOLIB_ERR_INVALID_CODING_RATE)
+  {
+    Serial.println(F("Selected coding rate is invalid for this module!"));
+    while (true)
+      ;
+  }
+
+  // Sets LoRa sync word. SX1278/SX1276/SX1268/SX1262/SX1280 : Sets LoRa sync word. Only available in LoRa mode.
+  if (radio.setSyncWord(0xAB) != RADIOLIB_ERR_NONE)
+  {
+    Serial.println(F("Unable to set sync word!"));
+    while (true)
+      ;
+  }
+
+  // Sets transmission output power. SX1262 :  Allowed values are in range from -9 to 22 dBm. This method is virtual to allow override from the SX1261 class.
+  if (radio.setOutputPower(CONFIG_RADIO_OUTPUT_POWER) ==
+      RADIOLIB_ERR_INVALID_OUTPUT_POWER)
+  {
+    Serial.println(F("Selected output power is invalid for this module!"));
+    while (true)
+      ;
+  }
+
+  Serial.print(F("Passed Radio Setpup ... "));
+  #endif
+
+  // * Initialise the MQTT client to communicate with the ThingsBoard server */
   client.set_server(server, port);
   client.set_data_callback(mqttCallback);
   // Set MQTT buffer sizes before connecting to broker
@@ -191,20 +296,23 @@ void setup() {
 }
 
 void loop() {
-    if (!client.connected()) {
+  // check if it is still connected to teh MQTT broker, if not try to reconnect
+  if (!client.connected()) {
     connectToMQTT();
   }
+  // Process incoming MQTT messages !!! ENSURE LIMITED BLOCKING SHIT AFTER IT !!!
   client.loop();
 
   unsigned long now = millis();
 
-  // Send telemetry
+  // Send telemetry - this could be swapped out for using the gps time im hoping i can use teh clock time things board to send interval 
   if (now - lastSent > samplingFrequency) {
-    sendTelemetry();
+    RecieveAndSendTelemetry();
     lastSent = now;
   }
 
-  // Re-request attribute every 30 seconds
+  // This is for checking if how often you want the the sensors to be read adn sent to the base station where frequency is 
+  // set on the thingsboard dashboard  
   if (now - lastRequest > 30000) {
     requestSharedAttribute();
     lastRequest = now;
