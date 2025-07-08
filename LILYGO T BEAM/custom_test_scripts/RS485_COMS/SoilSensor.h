892171b2874e06fc7e0baa7ba0c18872998c24d2
tphi219 <tphi219@aucklanduni.ac.nz>
2025-07-08 21:33:53 +1200

    Make library for handling rs485 coms with soil sensor UNTESTED but does compile so on god this be fire and will work

---
+++ LILYGO T BEAM/custom_test_scripts/RS485_COMS/SoilSensor.h
@@ -0,0 +1,36 @@
+// Header file for soild sensor helper functions using the RS485 Protocol
+#ifndef SOIL_SENSOR_H
+#define SOIL_SENSOR_H
+
+#include <Arduino.h>
+#include <LoRaBoards.h>
+
+class SoilSensor {
+  private: 
+    uint8_t DeviceAddress;
+    uint32_t baud = 9600; // Default baud rate for RS485 communication
+    int RS485_RX_PIN = 19;
+    int RS485_TX_PIN = 20;
+    int RE_DE_PIN = 21; // Needs to be selected
+
+    uint16_t Temp;
+    uint16_t Moisture;
+    uint16_t EC;
+    uint16_t PH;
+    // Helper functions for comms
+    void setTransmit(bool tx);
+    bool sendPacket(uint8_t *data, uint8_t len);
+    uint16_t crc16(uint8_t *buf, uint8_t len);
+    bool readResponse(uint8_t *buf, uint8_t len, uint16_t timeout_ms);
+  public:
+    SoilSensor(uint8_t address = 0x08) : DeviceAddress(address) {};
+
+    void begin();
+    void readSensor(uint8_t (&respone)[13]);
+    uint16_t GetTemperature(uint8_t (&response)[13]);
+    uint16_t GetMoisture(uint8_t (&response)[13]);
+    uint16_t GetEC(uint8_t (&response)[13]);
+    uint16_t GetPH(uint8_t (&response)[13]);
+};
+
+#endif
--- LILYGO T BEAM/custom_test_scripts/RS485_COMS/com.ino
+++ LILYGO T BEAM/custom_test_scripts/RS485_COMS/com.ino
@@ -1,21 +1,29 @@
 #include "LoRaBoards.h"
+#include "SoilSensor.h"
 
-// From my reading of code it seems that serial1 is gps so teh exposed tx adn rx pins are serial 2
-# define RS485_RX_PIN 19
-# define RS485_TX_PIN 20
-# define RS485_BAUD_RATE 9600
-uint8_t com[8] = {0x01,0x03,0x00,0x00,0x00,0x07,0x44,0x0C};
+// uint8_t com[8] = {DeviceAddress,FunctionCode,0x00,0x00,0x00,NumberOfRegisters,0x44,0x0C};
+SoilSensor FourParam;
+uint8_t resp[13];
 
 void setup() {
     setupBoards();  // Setup the Board (pretty sure its the pins)
     Serial.begin(115200);
-    Serial2.begin(RS485_BAUD_RATE, SERIAL_8N1, RS485_RX_PIN, RS485_TX_PIN);
+    FourParam.begin();
+    // Serial2.begin(RS485_BAUD_RATE, SERIAL_8N1, RS485_RX_PIN, RS485_TX_PIN);
 }
 
 void loop() {
   // For example, you could call setupBoards(), beginSDCard(), or other functions as needed.
-}
+    FourParam.readSensor(resp);
+    Serial.print("Temperature: ");
+    Serial.println(FourParam.GetTemperature(resp));
+    Serial.print("Moisture: ");
+    Serial.println(FourParam.GetMoisture(resp));
+    Serial.print("EC: ");
+    Serial.println(FourParam.GetEC(resp));
+    Serial.print("PH: ");
+    Serial.println(FourParam.GetPH(resp));
 
-void readSensor(void) {
-
+  
+  delay(3000);
 }
